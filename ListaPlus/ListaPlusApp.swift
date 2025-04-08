cdef8a0d7097ddaea8da03cf4868ad6d64b80a11
MuriloCC <mccampos1@stefanini.com>
2025-04-08 16:58:09 -0300

    Initial Commit

---
+++ ListaPlus/ListaPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListaPlusApp.swift
+//  ListaPlus
+//
+//  Created by Murilo Castilho on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ListaPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
